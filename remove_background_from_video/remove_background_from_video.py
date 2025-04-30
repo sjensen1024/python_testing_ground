115c18d3f8a8e060e7852a06e1b4b726b3254ff8
sjensen1024 <stevenmjensen1024@gmail.com>
2025-04-29 21:06:06 -0500

    modified to output frames as images with removed backgrounds; added todo's for cleaner implementation

--- remove_background_from_video/remove_background_from_video.py
+++ remove_background_from_video/remove_background_from_video.py
@@ -1,6 +1,7 @@
 from rembg import remove
 from moviepy import VideoFileClip
 from PIL import Image 
+import numpy
 
 def get_number_of_digits(number):
     return len(str(abs(number)))
@@ -14,21 +15,28 @@ def get_frame_file_name(current_frame, number_of_frames, output_directory):
     prefix = '0' * (duration_digits - frame_digits)
     return file_name + prefix + str(current_frame) + '.png'
 
-def extract_original_frames_as_png(input_video_path, original_frame_dir):
+def extract_frames_and_remove_backgrounds(input_video_path, original_frame_dir, output_video_path, transparent_frame_dir):
     original_video =  VideoFileClip(input_video_path) 
     count = 0
     for frame in original_video.iter_frames():
         count += 1
-        frame_file_name = get_frame_file_name(count, original_video.n_frames, original_frame_dir)
-        current_time_in_seconds = count/original_video.fps
-        original_video.save_frame(frame_file_name, t = current_time_in_seconds)
+        #input_file_name = get_frame_file_name(count, original_video.n_frames, original_frame_dir)
+        transparent_file_name = get_frame_file_name(count, original_video.n_frames, transparent_frame_dir)
+        #current_time_in_seconds = count/original_video.fps
+        #original_video.save_frame(frame_file_name, t = current_time_in_seconds)
+        frame_as_image = Image.fromarray(numpy.uint8(frame))
+        transparent_frame = remove(frame_as_image)
+        transparent_frame.save(transparent_file_name)
 
 def run():
     input_video_path =  'test_input.mp4' 
     output_video_path = 'test_output.mp4' 
     original_frame_dir = './original_frames/'
     transparent_frame_dir = './transparent_frames/'
-    extract_original_frames_as_png(input_video_path, original_frame_dir)
-    # TODO: Implement the removal of the background of each extracted image.
+    extract_frames_and_remove_backgrounds(input_video_path, original_frame_dir, output_video_path, transparent_frame_dir)
+    # TODO: Refactor to do the following:
+    #       1) Read the input/output paths from a config
+    #       2) Allow to optionally write both transparent and non-transparent versions instead of just transparent.
+    #       3) Combine the frames into a result .mp4
 
 run()
