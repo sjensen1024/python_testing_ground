fa6bd443989ce7175001b7cf4722c5d77e04bc72
sjensen1024 <stevenmjensen1024@gmail.com>
2025-05-06 21:03:09 -0500

    successfully exported frames to video

--- remove_background_from_video/remove_background_from_video.py
+++ remove_background_from_video/remove_background_from_video.py
@@ -1,15 +1,18 @@
 from rembg import remove
-from moviepy import VideoFileClip
+import moviepy
 from PIL import Image 
 import numpy
 import yaml
 
 def run():
     config = setup_config()
-    extract_frames_and_remove_backgrounds(config)
+    original_video = moviepy.VideoFileClip(config.get('input_file')) 
+    transparent_frame_images = extract_frames_and_remove_backgrounds(config, original_video)
+    transparent_sequence = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(transparent_frame_images, int(original_video.fps))
+    transparent_sequence.write_videofile(config.get('output_file'))
     # TODO: Refactor and make the code do the following:
-    #       1) Combine the frames into a result .mp4
-    #       2) Clear out the input/output directories if there are already existing files, as well as the result.
+    #       1) Clear out the input/output directories if there are already existing files, as well as the result.
+    #       2) Refactor. This could be a LOT cleaner.
 
 def setup_config():
     with open("./config.yml", 'r') as stream:
@@ -19,16 +22,26 @@ def setup_config():
       'transparent_frames_directory': config.get('transparent_frames_directory'),
       'input_file': config.get('input_file'),
       'output_file': config.get('output_file'),
-      'save_original_frames': config.get('save_original_frames')
+      'save_original_frames': config.get('save_original_frames'),
+      'save_transparent_frames': config.get('save_transparent_frames'),
+      'verbose_output': config.get('verbose_output')
     }
 
-def extract_frames_and_remove_backgrounds(config):
-    original_video =  VideoFileClip(config.get('input_file')) 
+def extract_frames_and_remove_backgrounds(config, original_video):
     count = 0
+    transparent_frame_images = []
     for frame in original_video.iter_frames():
         count += 1
         frame_file_name = get_frame_file_name(count, original_video.n_frames)
-        export_frame_as_images(frame, config, frame_file_name)
+        original_image = get_frame_as_image(frame)
+        save_original_image_if_should(original_image, config, config.get('original_frames_directory') + frame_file_name)
+        image_without_background = remove(original_image)
+        save_transparent_image_if_should(image_without_background, config, config.get('transparent_frames_directory') + frame_file_name)
+        transparent_frame_images.append(numpy.array(image_without_background))
+        if config.get('verbose_output'):
+            print('Finished processing frame ' + str(count))
+    return transparent_frame_images
+
 
 def get_frame_file_name(current_frame, number_of_frames):
     file_name = '_'
@@ -39,17 +52,20 @@ def get_frame_file_name(current_frame, number_of_frames):
     prefix = '0' * (duration_digits - frame_digits)
     return file_name + prefix + str(current_frame) + '.png'
 
-def export_frame_as_images(frame, config, file_name):
-    original_image = get_frame_as_image(frame)
-    if config.get('save_original_frames'):
-        original_image.save(config.get('original_frames_directory') + file_name)
-    image_without_background = remove(original_image)
-    image_without_background.save(config.get('transparent_frames_directory') + file_name)
-
 def get_frame_as_image(frame):
     return Image.fromarray(numpy.uint8(frame))
 
 def get_number_of_digits(number):
     return len(str(abs(number)))
+
+def save_original_image_if_should(image, config, save_path):
+    if not config.get('save_original_frames'):
+        return
+    image.save(save_path)
+
+def save_transparent_image_if_should(image, config, save_path):
+    if not config.get('save_transparent_frames'):
+        return
+    image.save(save_path)
 
 run()
