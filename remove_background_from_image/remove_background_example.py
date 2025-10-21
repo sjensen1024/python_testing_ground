7cfb809b81a482d9216a2e974d2bf6efaa3a9556
sjensen1024 <stevenmjensen1024@gmail.com>
2025-10-21 07:03:54 -0500

    open image in test

--- remove_background_from_image/remove_background_example.py
+++ remove_background_from_image/remove_background_example.py
@@ -1,10 +1,18 @@
 from rembg import remove 
 from PIL import Image 
+import time
   
 input_path =  'test_input.jpg' 
 output_path = 'test_output.png' 
   
 input = Image.open(input_path) 
+input.show()
+time.sleep(3)
+
+
 output = remove(input) 
-  
-output.save(output_path) 
+output.save(output_path)
+
+new_image = Image.open(output_path)
+new_image.show()
+time.sleep(3)
